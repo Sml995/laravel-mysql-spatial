7cda655ae65dc2f6494c67d308ddadc3095d2e52
Samuel HASSID <samuel@supplement-bacon.com>
2024-09-25 23:18:00 +0200

    Update Blueprint.php

--- src/Schema/Blueprint.php
+++ src/Schema/Blueprint.php
@@ -9,14 +9,15 @@ class Blueprint extends IlluminateBlueprint
     /**
      * Add a geometry column on the table.
      *
-     * @param string   $column
-     * @param null|int $srid
+     * @param string      $column
+     * @param null|string $subtype
+     * @param null|int    $srid
      *
      * @return \Illuminate\Support\Fluent
      */
-    public function geometry($column, $srid = null)
+    public function geometry($column, $subtype = null, $srid = null)
     {
-        return $this->addColumn('geometry', $column, compact('srid'));
+        return $this->addColumn('geometry', $column, compact('subtype', 'srid'));
     }
 
     /**
